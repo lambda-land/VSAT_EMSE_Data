ab7941b26722b7b66a83748e7740d18e5f12a55f
doyougnu <jmy6342@gmail.com>
2021-10-02 10:46:37 -0700

    add nix folder

---
+++ nix/R.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? import ./nixpkgs-01-10.nix }:
+let
+  pkgs   = import nixpkgs {};
+  stdenv = pkgs.stdenv;
+in with pkgs; {
+  myProject = stdenv.mkDerivation {
+    name = "Vsat_Data_Analysis";
+    version = "2";
+    src = if pkgs.lib.inNixShell then null else nix;
+
+    buildInputs = with rPackages; [
+      pandoc
+      R
+      ggplot2
+      knitr
+      rmarkdown
+      devtools
+      reshape2
+      yaml
+      optparse
+      tidyr
+      dplyr
+      svglite
+      cowplot
+      latex2exp
+      gridExtra
+      tidyverse
+      forcats
+      Hmisc
+      ggpubr
+      rstatix
+    ];
+  };
+}
